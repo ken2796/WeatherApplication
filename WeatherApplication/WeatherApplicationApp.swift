4d2449a44785614ef3ec50deca473e211adbd3e3
KennethF <139214517+KennethFrancia@users.noreply.github.com>
2024-12-16 15:43:22 +0800

    Initial Commit

---
+++ WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by Kenneth Francia on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
